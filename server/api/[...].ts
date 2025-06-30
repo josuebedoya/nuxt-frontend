9888e067e32fe48d8e6b000596e888e13630d4fc
msolanogithub <msolano2505@gmail.com>
2025-06-30 18:59:46 -0500

    added proxy handler for external backend

--- server/api/[...].ts
+++ server/api/[...].ts
@@ -1,10 +1,15 @@
 import { joinURL } from 'ufo'
 
 export default defineEventHandler(async (event) => {
-  // Get the runtimeconfig proxy url
+  const {token} = JSON.parse(<string>getCookie(event, 'auth') ?? '{}')
+  // Get the runtime config proxy url
   const proxyUrl = useRuntimeConfig().apiBase
   // check the path
   const target = joinURL(proxyUrl, event.path)
+  // Set Authorization header if token exists
+  if (token) {
+    event.node.req.headers.authorization = `Bearer ${token}`
+  }
   // proxy it!
   return proxyRequest(event, target)
 })
