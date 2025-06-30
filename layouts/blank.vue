89920e821b4cc198250b9f21901013f086bfa021
msolanogithub <msolano2505@gmail.com>
2025-06-30 18:58:29 -0500

    added ColorModeButton component and implemented theme toggle in blank.vue layout

---
+++ layouts/blank.vue
@@ -0,0 +1,9 @@
+<template>
+    <div>
+      <!-- Top-right theme toggle -->
+      <div class="absolute top-4 right-4 z-50">
+        <ColorModeButton />
+      </div>
+      <slot />
+    </div>
+</template>
