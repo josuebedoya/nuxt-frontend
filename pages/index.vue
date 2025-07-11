6ba30808b62e48386270791fb604651692e820e4
msolanogithub <msolano2505@gmail.com>
2025-07-11 18:29:12 -0500

    fix: update authentication token handling and enhance home page greeting

--- pages/index.vue
+++ pages/index.vue
@@ -1,6 +1,11 @@
 <template>
   <div>
-    <h1>Welcome to the home page</h1>
+    <h1>
+      Welcome to the home page
+      <ClientOnly>
+        <b v-if="authStore.isAuthenticated">[{{ authStore.user?.firstName || '-' }}]</b>
+      </ClientOnly>
+    </h1>
     <UButton block color="neutral" variant="soft" label="Logout" @click="authStore.logout"/>
   </div>
 </template>
--- server/api/[...].ts
+++ server/api/[...].ts
@@ -1,14 +1,14 @@
 import { joinURL } from 'ufo'
 
 export default defineEventHandler(async (event) => {
-  const {token} = JSON.parse(<string>getCookie(event, 'auth') ?? '{}')
+  const {token} = JSON.parse(<string>getCookie(event, 'iuser-auth') ?? '{}')
   // Get the runtime config proxy url
   const proxyUrl = useRuntimeConfig().apiBase
   // check the path
   const target = joinURL(proxyUrl, event.path)
   // Set Authorization header if token exists
-  if (token) {
-    event.node.req.headers.authorization = `Bearer ${token}`
+  if (token && token.accessToken) {
+    event.node.req.headers.authorization = `Bearer ${token.accessToken}`
   }
   // proxy it!
   return proxyRequest(event, target)
