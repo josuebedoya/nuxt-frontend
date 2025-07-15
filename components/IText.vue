1d1da2a0ad66c27dc18b955f163db0eff283e19f
adrianacardoso <acardosof27@gmail.com>
2025-07-15 13:49:46 -0400

    component imedia

---
+++ components/IText.vue
@@ -0,0 +1,7 @@
+<template>
+  <div class="absolute inset-0 bg-black/70 text-white flex flex-col items-center justify-center text-center p-4">
+    <h2 class="text-xl font-bold">Título desde componente</h2>
+    <p class="text-sm">Subtítulo y más contenido</p>
+    <button class="mt-4 px-3 py-1 bg-white text-black rounded">Ver más</button>
+  </div>
+</template>
