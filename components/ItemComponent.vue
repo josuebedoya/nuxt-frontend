d92f610d534a6dad6ba494605c5f1065f690a612
TJULIAN-D <juliandavidbonillatorres@gmail.com>
2025-07-16 15:05:43 -0500

    component item

---
+++ components/ItemComponent.vue
@@ -0,0 +1,52 @@
+<!--
+Ítem: elemento para mostrar la información de 1 elemento. Cuenta con ordenamiento por cada subelemento. con estos sub elementos:
+
+imagen / galería / video (posiblemente el componente de imagen)
+
+título -> style escrito tenga N tamaño , para mobil otro…
+
+descripción
+
+botón de acción
+
+SubLabels (texto pequeño) posiblemente 3? [usuario, fecha, categoría] o sub Label opcionales sin restricción de cantidad
+-->
+
+<script lang="ts" setup>
+import { button } from "#build/ui";
+
+const data = {
+  img: "https://dev-roomkaraoke.ozonohosting.com/assets/media/sedes/zona-rosa.jpg?u=1749163822",
+  title: "Este es el titulo",
+  labels: {
+    resumen:
+      "Lorem ipsum dolor sit amet consectetur, adipisicing elit. Laboriosam officia consectetur aliquam tenetur officiis ratione eveniet, eaque consequuntur eius nemo optio dolore quasi blanditiis! Saepe excepturi illo at. Quasi, est?",
+    description:
+      "Eeaque consequuntur eius nemo optio dolore quasi blanditiis! Saepe excepturi illo at. Quasi, est? Lorem ipsum dolor sit amet consectetur, adipisicing elit. Laboriosam officia consectetur aliquam tenetur officiis ratione eveniet, ",
+  },
+  button: "Ver más",
+};
+</script>
+
+
+<template>
+  <section class="card-item">
+    <!--component image-->
+
+    <img :src="data.img" alt="" style="aspect-ratio: 1/1; height: 200px" />
+    <div class="title">{{ data.title }}</div>
+    <div v-for="(label, key) in data.labels" :key="key">
+      <div class="{{ label }}">
+        {{ label }}
+      </div>
+    </div>
+    <UButton> {{ data.button }} </UButton>
+  </section>
+</template>
+
+
+
+
+
+<style>
+</style>
--- pages/index.vue
+++ pages/index.vue
@@ -2,14 +2,39 @@
   <div>
     <h1>
       Welcome to the home page
+      <div class="grid grid-cols-4 gap-4">
+        <div v-for="(item, key) in items" :key="key">
+          <ItemComponent />
+          <!-- <div>{{ item }}</div> -->
+        </div>
+      </div>
+
       <ClientOnly>
-        <b v-if="authStore.isAuthenticated">[{{ authStore.user?.firstName || '-' }}]</b>
+        <b v-if="authStore.isAuthenticated"
+          >[{{ authStore.user?.firstName || "-" }}]</b
+        >
       </ClientOnly>
     </h1>
-    <UButton block color="neutral" variant="soft" label="Logout" @click="authStore.logout"/>
+    <UButton
+      block
+      color="neutral"
+      variant="soft"
+      label="Logout"
+      @click="authStore.logout"
+    />
   </div>
 </template>
 
 <script setup>
-const authStore = useIuserAuthStore()
+const authStore = useIuserAuthStore();
+const items = {
+  item1: {},
+  item2: {},
+  item3: {},
+  item4: {},
+  item5: {},
+  item6: {},
+  item7: {},
+  item8: {},
+};
 </script>
