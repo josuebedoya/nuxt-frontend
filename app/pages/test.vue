725a9fc30a706bd2e5333a91d39989c531e7b9f0
adrianacardoso <acardosof27@gmail.com>
2025-07-16 18:48:47 -0400

    IMedia Imagen + Video

--- app/pages/test.vue
+++ app/pages/test.vue
@@ -1,12 +1,26 @@
 <template>
-  <div class="container m-auto py-5">
+  <div class="container m-auto py-5 bg-amber-50">
     <h1 class="text-center font-bold text-lg">Componente IMedia</h1>
-    <div class="my-5 grid gap-4 grid-cols-3">
+    <div class="my-5 grid gap-4 grid-cols-1 sm:grid-cols-3">
+      <IMedia 
+        base-src="https://dev-burgertour.ozonohosting.com/assets/media/video11.mp4?u=1751481253"
+        :isVideo="true"
+        :videoAttrs="{ controls: true, autoplay: true, muted: true, loop: true, playsinline: true }"
+        alt="Promo"
+        ratio="4/5"
+        width="w-full"
+        rounded="rounded-xl"
+      />
+
       <IMedia
         base-src="https://dev-surtiplate.ozonohosting.com/assets/media/general/inicio/slider-1_extraLargeThumb.webp"
         overlay-hide-on-hover 
         alt="Ejemplo"
         object="contain"
+        width="w-full"
+        ratio="auto" 
+        height="h-80"
+        rounded="rounded-2xl"
         overlay-gradient="linear-gradient(to right, rgba(0,0,0,0.6), transparent)" />
 
 
@@ -15,12 +29,16 @@
         hover-src="https://dev-surtiplate.ozonohosting.com/assets/media/general/productos/img-2_extraLargeThumb.webp"
         alt="Ejemplo" 
         ratio="8/3" 
+        class="shadow-2xl border border-gray-200"
+        rounded="rounded-s-full"
         overlay-gradient="linear-gradient(230deg, #CDDC39, transparent)"/>
 
       <IMedia
         base-src="https://dev-surtiplate.ozonohosting.com/assets/media/general/inicio/slider-1_extraLargeThumb.webp"
         hover-src="https://dev-surtiplate.ozonohosting.com/assets/media/general/productos/img-2_extraLargeThumb.webp"
-        ratio="4/5">
+        ratio="4/5"
+        rounded="rounded-e-lg"
+        >
         <template #overlay>
           <IText />
         </template>
--- components/IMedia.vue
+++ components/IMedia.vue
@@ -1,29 +1,45 @@
 <template>
   <figure
-    class="relative group overflow-hidden rounded-xl"
+    class="relative group overflow-hidden"
+    :class="[rounded, width, height, $attrs.class]"
     :style="{ aspectRatio: ratio }"
   >
-    <!-- Imagen base (si no hay hover) -->
-    <img
-      v-if="!hoverSrc"
-      :src="baseSrc"
-      :alt="alt"
-      class="absolute inset-0 w-full h-full z-0"
-      :class="'object-' + object"
-    />
+    <!-- Media base (imagen o video si no hay hover) -->
+    <template v-if="!hoverSrc">
+      <!-- VIDEO -->
+      <video
+        v-if="isVideo"
+        :src="baseSrc"
+        v-bind="videoAttrs"
+        class="absolute inset-0 w-full h-full z-0 rounded-inherit"
+        :class="'object-' + object"
+      ></video>
+
+      <!-- IMAGEN -->
+      <img
+        v-else
+        :src="baseSrc"
+        :alt="alt"
+        :fetchpriority="fetchpriority"
+        class="absolute inset-0 w-full h-full z-0 rounded-inherit"
+        :class="'object-' + object"
+      />
+    </template>
 
     <!-- Imagen base y hover  -->
     <template v-else>
       <img
         :src="baseSrc"
         :alt="alt"
-        class="absolute inset-0 w-full h-full z-0 transition-opacity duration-500 opacity-100 group-hover:opacity-0"
+        :fetchpriority="fetchpriority"
+        class="absolute inset-0 w-full h-full z-0 transition-opacity duration-500 opacity-100 group-hover:opacity-0 rounded-inherit"
         :class="'object-' + object"
       />
       <img
         :src="hoverSrc"
         :alt="alt + ' hover'"
-        class="absolute inset-0 w-full h-full z-10 transition-opacity duration-500 opacity-0 group-hover:opacity-100"
+        :fetchpriority="fetchpriority"
+        class="absolute inset-0 w-full h-full z-10 transition-opacity duration-500 opacity-0 group-hover:opacity-100 rounded-inherit"
         :class="'object-' + object"
       />
     </template>
@@ -39,7 +55,7 @@
     <!-- Gradiente en hover si se define -->
     <div
       v-if="overlayGradientHover"
-      class="absolute inset-0 z-20 transition-opacity duration-500 opacity-0 group-hover:opacity-100"
+      class="absolute inset-0 z-20 transition-opacity duration-500 opacity-0 group-hover:opacity-100 "
       :style="{ background: overlayGradientHover }"
     ></div>
 
@@ -60,10 +76,16 @@ const props = defineProps({
   object: { type: String, default: 'cover' },
   overlayGradient: String,
   overlayGradientHover: String,
-  overlayHideOnHover: {
-    type: Boolean,
-    default: false
-  }
+  overlayHideOnHover: { type: Boolean, default: false },
+  rounded: { type: String, default: 'rounded-none' },
+  width: { type: String, default: 'w-full' },
+  height: { type: String, default: 'h-auto' },
+  isVideo: { type: Boolean, default: false },
+  videoAttrs: { type: Object, default: () => ({}) },
+  fetchpriority: { type: String, default: 'auto' } // puede ser: 'high', 'low' o 'auto'
 })
+
+
+
 
 </script>
