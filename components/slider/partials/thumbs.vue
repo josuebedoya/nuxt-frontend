e91686d540d0f4cbe98b6a9d0c3949e5fbaf504f
josuebedoya348 <josuebedoya348@gmail.com>
2025-07-29 18:27:11 -0500

    + Add Carousel to Thumbs && handler measurements from container items

--- components/slider/partials/thumbs.vue
+++ components/slider/partials/thumbs.vue
@@ -1,37 +1,36 @@
 <script setup lang="ts">
-defineProps({
+const props = defineProps({
  items: {type: Array, required: true},
- componentThumb: {type: null, required: true},
+ component: {type: null, required: true},
  controls: {type: Object, required: true},
  select: {type: Function, required: true},
-
- isVertical: {type: Boolean, default: false},
- withNavs: {type: Boolean, default: false},
- withDots: {type: Boolean, default: false},
-
- itemClass: {type: String, default: ""},
- itemActiveClass: {type: String, default: ""},
- breakpoint: {type: Object, default: {0: 2, "md": 3, "xl": 4}},
- itemPadding: {type: String, default: "p-2"},
- moveWithWheel:{type: Boolean, default: false},
+ moveOnOver: {type: Boolean, default: false},
+ thumbsProps: {type: Object, default: {}},
 })
+
+const activeThumbs = inject<Ref<number[]>>('activeThumbs')
+
+const sliderProps = {
+ viewportClass: `thumbs-${props.thumbsProps?.isVertical ? 'v' : 'h'}-container`,
+ autoDimensionedViewport: true,
+
+ breakPoint: props.thumbsProps?.breakPoint ?? {0: 2, "md": 3, "xl": 4},
+ classItem: 'max-w-max thumb' + (props.thumbsProps?.classItem ?? ''),
+ classItemActive: 'thumb-active' + (props.thumbsProps?.classItemActive ?? ''),
+ paddingItems: props.thumbsProps?.paddingItems ?? (props.thumbsProps?.isVertical ? 'py-3' : 'px-3'),
+ autoPlay: props.thumbsProps?.autoPlay ?? true,
+ pauseOnHover: props.thumbsProps?.pauseOnHover ?? true,
+ dragFree: props.thumbsProps?.dragFree ?? true,
+ moveWithWheel: props.thumbsProps?.moveWithWheel ?? true,
+ speed: props.thumbsProps?.speed ?? 0.6,
+ ...props.thumbsProps
+}
 </script>
 
 <template>
  <Slider
    :items="items"
-   :component="componentThumb"
-   :auto-play="false"
-   items-center
-   :break-point="breakpoint"
-   :with-dots="withDots"
-   :with-navs="withNavs"
-   class-item="max-w-max"
-   :viewport-class="`thumbs-${isVertical ? 'v' : 'h'}-container`"
-   auto-dimensioned-viewport
-   :is-vertical="isVertical"
-   :padding-items="itemPadding"
-   :move-with-wheel="moveWithWheel"
-   centered-items
+   :component="component"
+   v-bind="sliderProps"
  />
 </template>
