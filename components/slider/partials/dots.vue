6c43fc3b6c9b64a8f85f9b0c50445818eac47970
josuebedoya348 <josuebedoya348@gmail.com>
2025-08-01 17:36:37 -0500

    ↔ Fix dots to typescript props

--- components/slider/partials/dots.vue
+++ components/slider/partials/dots.vue
@@ -1,44 +1,59 @@
 <script setup lang="ts">
-const props = defineProps({
- dots: {type: Array, required: true},
- containerClass: {type: [String, Array, Object], default: 'm-2'},
- scrollTo: {type: Function, required: true},
- vertical: {type: Boolean, default: false},
- dotClass: {type: [String, Array, Object], default: ''},
- size: {type: String, default: 'sm'},
- variant: {type: String, default: 'solid'},
- type: {type: String, default: 'rounded'},
- color: {type: String, default: 'primary'},
+import variables from "./availableVariables";
+
+interface dotsProps {
+ dots: Array<number>,
+ containerClass?: string | string[] | Record<string, boolean>,
+ scrollTo: (index: number) => void,
+ vertical?: boolean,
+ dotClass?: string | string[] | Record<string, boolean>
+ size?: string,
+ variant?: 'outline' | 'solid',
+ type?: 'square' | 'rounded' | 'bars',
+ color?: string,
+ margin?: string,
+}
+
+const props = withDefaults(defineProps<dotsProps>(), {
+ vertical: false,
+ size: 'sm',
+ variant: 'solid',
+ type: 'rounded',
+ color: 'primary',
+ margin: 'm-1'
 })
+
 const activeDot = inject<Ref<number[]>>('selectedIndex') || 0;
-const avaibleSizes = ['sm', 'md', 'lg', 'xl'];
-const classDot = computed(() => {
- return [
-  'dot',
-  avaibleSizes.includes(props.size) && '!py-0 px-[2px]',
-  !avaibleSizes.includes(props.size) && `${props.size} leading-none`,
-  props.type === 'bars' && `w-3.5 h-1 max-[768px]:max-w-2`,
-  props.type === 'rounded' && 'rounded-full',
-  props.type === 'square' && 'rounded-0',
-  props.variant === 'solid' && '[&:not(.active)]:bg-inverted',
-  props.variant === 'outline' && ` border-2`,
-  props.dotClass
- ].filter(Boolean)
-})
+const classDot = computed(() => ([
+ 'dot',
+ variables.sizes.includes(props.size) && '!py-0 px-[2px]',
+ !variables.sizes.includes(props.size) && `${props.size} leading-none`,
+ !variables.colors.includes(props.color) && props.color,
+ props.type === 'bars' && `w-3.5 h-1 max-[768px]:max-w-2`,
+ props.type === 'rounded' && 'rounded-full',
+ props.type === 'square' && '!rounded-none',
+ props.variant === 'solid' && '[&:not(.active)]:bg-inverted',
+ props.variant === 'outline' && ` border-2`,
+ props.margin,
+ props.dotClass
+].filter(Boolean)))
+const dotProps = computed(() => ({
+ color: props.color,
+ variant: props.variant,
+ size: props.size,
+ type: props.type
+}));
 </script>
 <template>
- <div class="dots flex justify-center items-center gap-3" :class="[containerClass, {'flex-col': vertical}]">
+ <div class="dots flex justify-center items-center gap-3 my-3" :class="[containerClass, {'flex-col mx-3': vertical}]">
   <div v-for="(_, index) in dots" :key="index">
    <UButton
      class="cursor-pointer max-[768px]:text-[10px] transition-all"
      :aria-label="`slide ${ index + 1 }`"
-     :color="color"
-     :variant="variant"
-     :size="size"
-     :square="variant === 'square'"
+     v-bind="dotProps"
      :class="[classDot,{'active': activeDot === index}]"
      :data-state="activeDot === index? 'active' : void 0"
-     @click="scrollTo(index) || void 0"
+     @click="scrollTo(index)"
    >
     <span class="relative block opacity-0">&#x25AA;&#x25AA;&#x25AA;</span>
    </UButton>
