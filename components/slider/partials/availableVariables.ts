71a1912030062e49edabf83a898316f876df5b72
josuebedoya348 <josuebedoya348@gmail.com>
2025-08-01 17:36:37 -0500

    ↔ Fix dots to typescript props

---
+++ components/slider/partials/availableVariables.ts
@@ -0,0 +1,10 @@
+const variables = {
+    colors: [
+        'primary', 'secondary', 'success', 'info', 'warning', 'error', 'neutral'
+    ],
+    sizes: [
+        'sm', 'md', 'lg', 'xl'
+    ]
+};
+
+export default variables;
