fe7b88517c3b54eddacc7c1d8852535dbb4f65e9
josuebedoya348 <josuebedoya348@gmail.com>
2025-07-31 21:29:44 -0500

    ↔ Fix sizes dots in mobile

--- components/slider/partials/dots.vue
+++ components/slider/partials/dots.vue
@@ -17,7 +17,7 @@ const classDot = computed(() => {
   'dot',
   avaibleSizes.includes(props.size) && '!py-0 px-[2px]',
   !avaibleSizes.includes(props.size) && `${props.size} leading-none`,
-  props.type === 'bars' && `w-3.5 h-1`,
+  props.type === 'bars' && `w-3.5 h-1 max-[768px]:max-w-2`,
   props.type === 'rounded' && 'rounded-full',
   props.type === 'square' && 'rounded-0',
   props.variant === 'solid' && '[&:not(.active)]:bg-inverted',
@@ -30,7 +30,7 @@ const classDot = computed(() => {
  <div class="dots flex justify-center items-center gap-3" :class="[containerClass, {'flex-col': vertical}]">
   <div v-for="(_, index) in dots" :key="index">
    <UButton
-     class="cursor-pointer max-[768px]:max-w-2 transition-all"
+     class="cursor-pointer max-[768px]:text-[10px] transition-all"
      :aria-label="`slide ${ index + 1 }`"
      :color="color"
      :variant="variant"
