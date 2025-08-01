2340209983e99214773b69aad21bc16e29027664
josuebedoya348 <josuebedoya348@gmail.com>
2025-08-01 17:41:21 -0500

    ↔ Fix navs to typescript props

--- components/slider/partials/navs.vue
+++ components/slider/partials/navs.vue
@@ -1,75 +1,93 @@
 <script setup lang="ts">
-const props = defineProps({
- containerClass: {type: [String, Array, Object], default: 'm-2'},
- position: {type: String, default: 'bottom-center'},
- scrollNext: {type: Function, required: true},
- scrollPrev: {type: Function, required: true},
- hiddenNext: {type: Boolean, default: false},
- hiddenPrev: {type: Boolean, default: false},
- navClass: {type: [String, Array, Object], default: ''},
- prev: {type: Object, default: {}},
- next: {type: Object, default: {}},
- size: {type: String, default: 'sm'},
- variant: {type: String, default: 'solid'},
- color: {type: String, default: 'primary'},
- nextIcon: {type: String, default: 'material-symbols:arrow-forward-ios-rounded'},
- prevIcon: {type: String, default: 'material-symbols:arrow-back-ios-new'},
- labelNext: {type: String, default: ''},
- labelPrev: {type: String, default: ''},
- margin: {type: String, default: 'm-5'},
+import variables from "./availableVariables";
+
+interface navProps {
+ containerClass?: string | string[] | Record<string, boolean>;
+ position?: string;
+ scrollNext: () => void;
+ scrollPrev: () => void;
+ hiddenNext?: boolean;
+ hiddenPrev?: boolean;
+ navClass?: string | string[] | Record<string, boolean>;
+ prev?: Record<string, any>;
+ next?: Record<string, any>;
+ size?: string;
+ variant?: string;
+ color?: string,
+ nextIcon?: string;
+ prevIcon?: string;
+ labelNext?: string;
+ labelPrev?: string;
+ margin?: string;
+}
+
+const props = withDefaults(defineProps<navProps>(), {
+ hiddenNext: false,
+ hiddenPrev: false,
+ position: 'bottom-center',
+ prev: () => ({}),
+ next: () => ({}),
+ size: 'sm',
+ variant: 'ghost',
+ color: 'primary',
+ nextIcon: 'material-symbols:arrow-forward-ios-rounded',
+ prevIcon: 'material-symbols:arrow-back-ios-new',
+ margin: 'm-2'
 })
 
 const canScrollNext = inject<Ref<number[]>>('canScrollNext') || false;
 const canScrollPrev = inject<Ref<number[]>>('canScrollPrev') || false;
 
-
 const classNavs = computed(() => {
- const avaibleSizes = ['sm', 'md', 'lg', 'xl'];
  return [
-  'nav',
-  !avaibleSizes.includes(props.size) && `${props.size} leading-none`,
+  'nav cursor-pointer rounded-full',
+  !variables.sizes.includes(props.size) && `${props.size} leading-none`,
+  !variables.colors.includes(props.color) && props.color,
   props.variant === 'outline' && ` border-2`,
   props.navClass,
   props.margin
  ].filter(Boolean)
 })
-const classContainer = computed(() => {
- const position = props.position;
- return [
-  'justify-center',
-  position.endsWith('left') && '!justify-start',
-  position.endsWith('right') && '!justify-end',
-  props.containerClass
- ].filter(Boolean)
-})
+const classContainer = computed(() => ([
+ 'justify-center',
+ props.position.endsWith('left') && '!justify-start',
+ props.position.endsWith('right') && '!justify-end',
+ props.containerClass
+].filter(Boolean)))
+const navProps = computed(() => ({
+ color: props.color,
+ variant: props.variant,
+ size: props.size,
+}));
 </script>
 
 <template>
  <div class="navs flex items-center" :class="classContainer">
-  <UButton v-if="!hiddenPrev"
-           :disabled="!canScrollPrev"
-           :icon="prevIcon"
-           :color="color"
-           :variant="variant"
-           aria-label="prev"
-           :label="labelPrev"
-           v-bind="typeof prev === 'object' ? prev : void 0"
-           :class="classNavs"
-           class="cursor-pointer"
-           @click="scrollPrev() || void 0"
+  <UButton
+    v-if="!hiddenPrev"
+    :disabled="!canScrollPrev"
+    :icon="prevIcon"
+    aria-label="prev"
+    :label="labelPrev"
+    v-bind="{
+      ...navProps,
+      ...prev,
+    }"
+    :class="classNavs"
+    @click="scrollPrev()"
   />
-  <UButton v-if="!hiddenNext"
-           :disabled="!canScrollNext"
-           :icon="nextIcon"
-           :color="color"
-           :size="size"
-           :variant="variant"
-           aria-label="next"
-           :label="labelNext"
-           v-bind="typeof next === 'object' ? next : void 0"
-           :class="classNavs"
-           class="cursor-pointer"
-           @click="scrollNext() || void 0"
+  <UButton
+    v-if="!hiddenNext"
+    :disabled="!canScrollNext"
+    :icon="nextIcon"
+    aria-label="next"
+    :label="labelNext"
+    v-bind="{
+     ...navProps,
+     ...next,
+    }"
+    :class="classNavs"
+    @click="scrollNext()"
   />
  </div>
 </template>
