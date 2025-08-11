0d985b8a9cfd8a2bcf306c10c444bfa01868c574
msolanogithub <msolano2505@gmail.com>
2025-08-11 17:52:18 -0500

    feat: made page module search custom pages in project not in module

--- modules/ipage/runtime/pages/[page-slug].vue
+++ modules/ipage/runtime/pages/[page-slug].vue
@@ -1,38 +1,42 @@
 <script setup lang="ts">
-import {ref,shallowRef} from 'vue'
-import {ipagePagesRepository} from '#ipage/utils/repository'
-import type {PageData} from '#ipage/types/pages.js'
+  import {ref, shallowRef} from 'vue'
+  import {ipagePagesRepository} from '#ipage/utils/repository'
+  import type {PageData} from '#ipage/types/pages.js'
 
-const route = useRoute()
-const page = ref<PageData>(null)
-const PageComponent = shallowRef(null)
-
-const pageSlug = route.params.slug
-
-const {data, error} = await useAsyncData('page', () =>
+  const route = useRoute()
+  const page = ref<PageData>()
+  const PageComponent = shallowRef()
+  const pageSlug = route.params.slug?.toString() ?? ''
+  const {data, error} = await useAsyncData('page', () =>
     ipagePagesRepository.show(pageSlug, {
       filter: {field: 'slug'}, include: 'translations'
     })
-)
+  )
 
-if (error.value) {
-  if (error.value?.statusCode === 404) {
-    throw createError({statusCode: 404, statusMessage: 'Page Not Found'})
+  if (error.value)
+  {
+    if (error.value?.statusCode === 404)
+    {
+      throw createError({statusCode: 404, statusMessage: 'Page Not Found'})
+    }
+    throw createError({statusCode: 500, statusMessage: 'Unexpected Error'})
   }
-  throw createError({statusCode: 500, statusMessage: 'Unexpected Error'})
-}
 
-page.value = data.value?.data
+  page.value = data.value?.data
 
-// Get all available component files in this folder
-const components = import.meta.glob('./*.vue')
-const componentPath = `./page-${page.value.id}.vue`
+  // Get all available component files in this folder
+  const components = import.meta.glob('@/pages/*.vue')
+  const componentPath = `/pages/page-${page.value?.id}.vue`
 
-if (components[componentPath]) {
-  PageComponent.value = defineAsyncComponent(components[componentPath])
-} else {
-  PageComponent.value = defineAsyncComponent(() => import('./default-page.vue'))
-}
+  if (components[componentPath])
+  {
+    PageComponent.value = defineAsyncComponent(
+      components[componentPath] as unknown as () => Promise<Component>
+    )
+  } else
+  {
+    PageComponent.value = defineAsyncComponent(() => import('./default-page.vue'))
+  }
 </script>
 
 <template>
